a52672eed882c1bd169d703fce41d797b59e271c
nicolas <nicolas.gautier@lapiscine.pro>
2023-05-10 16:19:55 +0200

    🔧 corriger(artwork.controller.js) : supprimer les lignes vides et les commentaires inutiles Le code a été nettoyé pour supprimer les lignes vides et les commentaires inutiles. Cela améliore la lisibilité du code et facilite la maintenance.

--- controllers/artwork.controller.js
+++ controllers/artwork.controller.js
@@ -3,12 +3,14 @@ const {ArtworkModel, ArtistModel, CategorieModel} = require('../db/sequelize')
 
 // Récupérer toutes les oeuvres
 exports.getAll = (req, res) => {
+  console.log('GET ALL')
+
   ArtworkModel.findAll({
     include: [ArtistModel, CategorieModel],
     order: [['id', 'DESC']]
   })
   .then((artwork) => {
-    const msg = "La liste des artwork a bien été trouvé"
+    const msg = "La liste des artworks a bien été trouvé"
     res.json({ msg, data: artwork })
   })
   .catch((error) => {
@@ -23,55 +25,89 @@ exports.getOne = (req, res) => {
     include: [ArtistModel.scope("withoutPassword"), CategorieModel]
   })
   .then((artwork) => {
-    if(artwork == null) {
+    if(!artwork) {
       const msg = "L'artwork n'existe pas"
       res.status(404).json({ msg })
     } else {
+      artwork.view_count += 1
+      artwork.save()
       const msg = "L'artwork à bien été récupéré"
       res.json({ msg, data: artwork })
     }
   })
   .catch(error => {
-    console.log(error)
     const msg = "L'artwork n'a pas pu être récuépré"
     res.status(500).json({msg, data: error})
   })
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // Créer une oeuvre
-exports.createOne = (req, res) => {  
+exports.createOne = (req, res) => {
   ArtworkModel.create(req.body)
-    .then((artwork) => {
-      if(artwork === null) {
-        const msg = "L'artwork n'est pas conforme"
-        res.status(404).json({ msg })
-      } else {
-          artwork.addArtists(req.headers.userid)
-          const msg = "L'oeuvre a bien été créée"
-          res.status(201).json({ msg, artwork })
-      }
+    .then(artwork => {
+      console.log(artwork + req.headers.userid)
+      
+      ArtistModel.findByPk(1)
+        .then((artist) => {
+          console.log(artist)
+          artist.addArtwork(artwork)
+          res.json({artwork})
+        })
+        .catch((error) => {
+          res.json("erreur dans addArtworks")
+        })      
     })
     .catch((error) => {
-      if (
-        error instanceof ValidationError ||
-        error instanceof UniqueConstraintError
-      ) {
-        return res.status(400).json({ message: error.message, data: error })
-      } 
-      const msg = "Impossible de créer l'artwork"
-        res.status(500).json({ msg, message: error.message, data: error })
+      res.json("erreur dans le catch ArtworksCreate")
     })
-}
+} 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 //  Modifier une oeuvre
 exports.updateOne = (req, res) => {
+  if(res.file){
+    req.body.src = req.file.originalname
+  }
+
   ArtworkModel.update(req.body, {
     where: {
       id: req.params.id,
     }
   })
     .then((artwork) => {
-      if(artwork === null) {
+      if(!artwork) {
         const msg = "L'oeuvre demandé n'existe pas."
         res.json({message: msg})
       } else {
@@ -93,21 +129,21 @@ exports.updateOne = (req, res) => {
 // Supprimer une oeuvre
 exports.deleteOne = (req, res) => {
   ArtworkModel.findByPk(req.params.id)
-  .then(artwork => {
-      if (artwork === null) {
-          const message = `Le artwork demandé n'existe pas.`
-          return res.status(404).json({ message })
-      }
-      return ArtworkModel.destroy({
-          where: {
-              id: req.params.id
-          }
-      })
-          .then(() => {
-              const message = `Le artwork ${artwork.name} a bien été supprimé.`
-              res.json({ message, data: artwork });
-          })
-  })
+    .then(artwork => {
+        if (!artwork) {
+            const message = `Le artwork demandé n'existe pas.`
+            return res.status(404).json({ message })
+        }
+        return ArtworkModel.destroy({
+            where: {
+                id: req.params.id
+            }
+        })
+            .then(() => {
+                const message = `Le artwork ${artwork.name} a bien été supprimé.`
+                res.json({ message, data: artwork });
+            })
+    })
   .catch(error => {
       const message = `Impossible de supprimer le artwork.`
       res.status(500).json({ message, data: error })
