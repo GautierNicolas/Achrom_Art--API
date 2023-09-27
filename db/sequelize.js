e29a0e2f046bdc682f16b533e5e29b928ede8105
Gautier.N <114164519+GautierNicolas@users.noreply.github.com>
2023-09-27 13:57:13 +0200

    Update sequelize.js

--- db/sequelize.js
+++ db/sequelize.js
@@ -42,61 +42,6 @@ const initDb = () => {
 
 sequelize
     .authenticate()
-    // // Création de 50 utilisateurs
-    // .then(() => {
-    //     for (let i = 0; i < 50; i++) {
-    //         ArtistModel.create({
-    //             artist_name: `artist${i}`,
-    //             email: `  ${ i }@gmail.com`,
-    //             password: bcrypt.hashSync('password', 10),
-    //             name : `name${i}`,
-    //             first_name : `first_name${i}`,
-    //             biography : `biography${i}`,
-    //             birth_date : `2020-10-10`
-    //         })
-    //     }
-    // })
-    // // Création de 50 oeuvres
-    // .then(() => {
-    //     for (let i = 0; i < 50; i++) {
-    //         ArtworkModel.create({
-    //             title: `artwork${i}`,
-    //             description: `artwork${i} description`,
-    //             src: `artwork${i}.jpg`,
-    //             year: 1990,
-            
-    //         })
-    //     }
-    // })
-    // // Création de 50 messages
-    // .then(() => {
-    //     for (let i = 0; i < 50; i++) {
-    //         MessageModel.create({
-    //             title: `message${i}`,
-    //             email: `${ i }@gmail.com`,
-    //             request: `message${i} content`,
-    //         })
-    //     }
-    // })
-    // bcrypt.hash('mdp', 10)
-    //     .then((hash) => {
-    //         ArtistModel.create({
-    //             artist_name: 'paul',
-    //             password: hash,
-    //             roles: ['artist', 'admin']
-    //         })
-    //     })
-    //     .catch(err => console.log(err))
-    // bcrypt.hash('mdp', 10)
-    // .then((hash) => {
-    //     ArtistModel.create({
-    //         artist_name: 'pierre',
-    //         password: hash,
-    //         roles: ['artist']
-    //     })
-    // })
-    // .catch(err => console.log(err))
-    // gestion des erreurs 
     .catch(error => console.log('Erreur lors de la connexion', error))
 
 module.exports = {
