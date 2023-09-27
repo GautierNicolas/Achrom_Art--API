b32083d4d7b7acc1b13db07f416446fca482d61c
Gautier.N <114164519+GautierNicolas@users.noreply.github.com>
2023-09-27 13:55:15 +0200

    Delete tests directory

--- tests/test-snippet.js
+++
@@ -1,8 +0,0 @@
-const { Sequelize } = require('sequelize')
-
-// Paramêtres Sequelize pour la connexion à la base de donnée setDatabseName
-const sequelize = new Sequelize('setDatabseName', 'setUserDB', 'setPasswordDB', {
-    host: 'localhost',
-    dialect: 'mariadb',
-    logging: false
-})
